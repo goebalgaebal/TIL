034ca73ffa2e0b32a2b7ecbf2d48c793673135ed
goebalgaebal123 <soyen_20@kookmin.ac.kr>
2019-06-24 18:40:50 +0900

    20190624 | mnist 데이터 활용0-9 숫자 판별 모델 구축 및 GUI로 결과 예측 구현

--- 빅데이터(딥러닝) 활용 AI 설계/딥러닝을 위한 빅데이터 기초/Python/code/Code14-05 머신러닝 scikit-learn MNIST.py
+++ 빅데이터(딥러닝) 활용 AI 설계/딥러닝을 위한 빅데이터 기초/Python/code/Code14-05 머신러닝 scikit-learn MNIST.py
@@ -1,25 +1,29 @@
 from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import math
 
-'''
-붓꽃 데이터 분류기 (머신러닝)
-개요 : 150개 붓꽃 정보(꽃받침 길이, 꽃받침 폭, 꽃잎 길이, 꽃잎 폭)
-종류 : 3개 (Iris-setosa, Iris-vesicolor, Iris-virginica)
-CSV 파일 : iris.csv
-'''
+def changeValue(list) :
+    # return [float(v) / 255 for v in list]
+    return [math.ceil(float(v) / 255) for v in list] # ceil : 올림
 
 # 0. 훈련 데이터, 테스트데이터 준비
-csv = pd.read_csv("./csv/iris.csv")
-data = csv.iloc[:, :-1]
-label = csv.iloc[:, [-1]]
+csv = pd.read_csv("./mnist/train_5K.csv")
+train_data = csv.iloc[:, 1:].values
+train_data = list(map(changeValue, train_data))
+train_label = csv.iloc[:, 0].values
+
+csv = pd.read_csv("./mnist/t10k_0.5K.csv")
+test_data = csv.iloc[:, 1:].values
+test_data = list(map(changeValue, test_data))
+test_label = csv.iloc[:, 0].values
 
 # 학습용, 훈련용 분리
-train_data, test_data, train_label, test_label = train_test_split(data, label, train_size=0.7)
+train_data, test_data, train_label, test_label = train_test_split(train_data, train_label, train_size=0.7)
 
 # 1. Classifire 생성(선택) → 머신러닝 알고리즘 선택
-clf = svm.SVC(gamma="auto")
-#clf = svm.NuSVC(gamma="auto")
+# clf = svm.SVC(gamma="auto")
+clf = svm.NuSVC(gamma="auto")
 
 # 2. 데이터로 학습 시키기
 # clf.fit([훈련 데이터], [정답])
@@ -32,5 +36,13 @@ print("정답률 : {0:.2f} %".format(score*100))
 
 # 3. 예측하기
 # clf.predit([예측할 데이터])
-result = clf.predict([[5.0, 3.2, 1.0, 0.2]])
-print(result)
+# result = clf.predict([[5.0, 3.2, 1.0, 0.2]])
+# print(result)
+
+
+# # 그림 사진 보기
+# import matplotlib.pyplot as plt
+# import  numpy as np
+# img = np.array(test_data[0]).reshape(28, 28)
+# plt.imshow(img, cmap = "gray")
+# plt.show()
